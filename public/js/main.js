c65bb0f1f0011b314a5dc90aceb8284b18c4bfe6
Elias Allman <allman.elias@gmail.com>
2020-03-16 12:19:10 -0700

    fixed error in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,20 @@
-asdfghjkl
+/* functions for general use*/
+
+/*This function returns the value associated with 'which param' on the URL*/
+function GetURLParameters(whichParam){
+  var pageURL = window.location.search.substring(1);
+  var pageURLVariables = pageURL.split('&');
+  for(var i =0; i < pageURLVariables.length; i++){
+    var parameterName = pageURLVariables[i].split('=');
+    if (parameterName[0]==whichParam){
+      return parameterName[1];
+    }
+  }
+}
+
+var username = GetURLParameters('username');
+if('undefined' == typeof username || !username){
+  username = 'Anonymous_'+Math.random();
+}
+
+$('#messages').append('<h4>' +username+'</h4>');
