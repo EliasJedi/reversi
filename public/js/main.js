be172d17c987f4a76a2a5876419fa611ab64b7e3
Elias Allman <allman.elias@gmail.com>
2020-04-20 11:20:41 -0700

    added illegal move logic as well as flipping logic includes a turn timer so you know how slow the op is

--- public/js/main.js
+++ public/js/main.js
@@ -258,6 +258,8 @@ var old_board = [
                 ];
 
 var my_color = ' ';
+var interval_timer;
+
 
 socket.on('game_update',function(payload){
 
@@ -288,8 +290,24 @@ socket.on('game_update',function(payload){
   }
 
   $('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
+  $('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>');
 
+  clearInterval(interval_timer);
+  interval_timer = setInterval( function(last_time){
+    return function(){
+      /*do the work of updating the ui*/
+      var d = new Date();
+      var elapsedmili = d.getTime() - last_time;
+      var minutes = Math.floor(elapsedmili / (60*1000));
+      var seconds = Math.floor((elapsedmili % (60*1000))/ 1000);
 
+      if (seconds < 10){
+        $('#elapsed').html(minutes+':0'+seconds);
+      }else{
+      $('#elapsed').html(minutes+':'+seconds);
+      }
+    }}(payload.game.last_move_time)
+    , 1000);
   /*animate changes to board*/
 
   var blacksum = 0;
@@ -336,10 +354,12 @@ socket.on('game_update',function(payload){
         else{
           $('#'+row+'_'+column).html('<img src="assets/images/error.gif" alt="error"/>');
         }
-
-        /*set up the interactivity*/
-        $('#'+row+'_'+column).off('click');
-        if(board[row][column] == ' '){
+}
+    /*set up the interactivity*/
+    $('#'+row+'_'+column).off('click');
+    $('#'+row+'_'+column).removeClass('hovered_over');
+    if(payload.game.whose_turn === my_color){
+      if(payload.game.legal_moves[row][column] === my_color.substring(0,1)){
           $('#'+row+'_'+column).addClass('hovered_over');
           $('#'+row+'_'+column).click(function(r,c){
             return function(){
@@ -351,8 +371,6 @@ socket.on('game_update',function(payload){
               socket.emit('play_token',payload);
             };
           }(row,column));
-        }else {
-          $('#'+row+'_'+column).removeClass('hovered_over');
         }
       }
     }
